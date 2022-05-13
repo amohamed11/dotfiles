843349b12859ab8eb9077da9982c0f01457bc50c
Anas Mohamed <14171441+amohamed11@users.noreply.github.com>
2022-05-13 09:32:41 -0600

    nvim simplified. the cycle continues.

--- nvim/lua/custom/init.lua
+++
@@ -1,9 +0,0 @@
-require "custom.plugins"
-
-local hooks = require "core.hooks"
-
--- MAPPINGS
--- To add new plugins, use the "setup_mappings" hook,
-
-hooks.add("setup_mappings", function(map)
-end)
